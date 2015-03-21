4183cea780febbadffcc9765d1d3b6b24957352f
Arianon <arianon@outlook.com>
2015-03-21 17:29:36 -0430

    Added deploy.sh  and a list of pkgs I want to keep

---
+++ deploy.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+CONFS=${XDG_CONFIG_HOME:-"$HOME/.config"}
+
+# Files and folders that belong to $XDG_CONFIG_HOME
+
+CONFIG_STUFF=(
+	awesome
+	bspwm
+	compton.conf
+	mpv
+	redshift.conf
+)
+
+# Files and folders that belong to $HOME
+
+HOME_STUFF=(
+	bashrc
+	dunstrc
+	irssi
+	ncmpcpp
+	nvimrc
+	tmux.conf
+	spectrwm.conf
+	xinitrc
+	Xresources
+	zshrc
+)
+
+simulate() {
+	echo "The following files and folder will be linked."
+	for f in ${CONFIG_STUFF[@]}; do
+		echo "$CONFS/$f -> $PWD/$f"
+	done 
+
+	for f in ${HOME_STUFF[@]}; do
+		echo "$HOME/.$f -> $PWD/$f"
+	done
+}
+
+link() {
+	read -p "Hit ^C to abort."
+	for f in ${CONFIG_STUFF[@]}; do
+		ln -s "$PWD/$f" "$CONFS/$f"
+	done 
+
+	for f in ${HOME_STUFF[@]}; do
+		ln -s "$PWD/$f" "$HOME/.$f"
+	done
+}
+
+simulate
