f058fe345423a8662b08a07edb102fa1372ecc12
Michael Valera <arianon@openmailbox.org>
2016-01-12 14:34:07 -0430

    history-substring-search: Proper configuration

--- .zsh/extensions/history-substring-search.zsh
+++ .zsh/extensions/history-substring-search.zsh
@@ -1,1 +1,11 @@
 source ${0:h}/history-substring-search/zsh-history-substring-search.zsh || return 1
+
+HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND='bg=green,fg=black'
+HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND='bg=red,fg=black'
+
+
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
+
+bindkey -M emacs '^P' history-substring-search-up
+bindkey -M emacs '^N' history-substring-search-down
