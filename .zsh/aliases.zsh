31d4312adfb566a5c431105d7c2cc1451026394c
Michael Valera <arianon@openmailbox.org>
2016-01-13 00:03:26 -0430

    Spaces to Tabs

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -26,11 +26,11 @@ if (( $+commands[aura] )) {
 }
 
 if (( $+commands[ls++] )) {
-  alias ls="ls++"
-  alias la="ls++ -A"
+	alias ls="ls++"
+	alias la="ls++ -A"
 } else {
-  alias ls="ls -BFX --group-directories-first --color=auto"
-  alias l="ls -goh"
-  alias la="ls -Ah"
-  alias ll="ls -Agoh"
+	alias ls="ls -BFX --group-directories-first --color=auto"
+	alias l="ls -goh"
+	alias la="ls -Ah"
+	alias ll="ls -Agoh"
 }
