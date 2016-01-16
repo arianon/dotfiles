f377978351eccd54e9387fa0281c90b899785f6b
Michael Valera <arianon@openmailbox.org>
2016-01-16 13:03:21 -0430

    Improving zsh

--- .zsh/config.zsh
+++ .zsh/config.zsh
@@ -12,12 +12,13 @@ setopt interactivecomments
 setopt correct
 setopt autocd
 setopt complete_in_word
-setopt auto_menu
 setopt hist_ignore_dups
 setopt inc_append_history
 setopt extended_history
 setopt share_history
 setopt extended_glob
+setopt menu_complete
+setopt nocasematch
 
 # Keybindings
 bindkey -e
@@ -33,6 +34,8 @@ autoload -Uz compinit && compinit
 zstyle ':completion:*' menu select=2 eval "$(dircolors -b)"
 zstyle ':completion:*' completer _complete _correct _approximate
 zstyle ':completion:*' expand prefix suffix
+# Case-and-hyphen insensitive completion
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z-_}={A-Za-z_-}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 
 # Modules
 
