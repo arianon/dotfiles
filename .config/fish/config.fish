b791409e685e7b00a84610a600f0cb7423576542
Michael Valera <arianon@openmailbox.org>
2016-02-24 05:13:58 -0430

    more updates to fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,12 @@
-set fish_color_normal normal
-
+set fish_color_autosuggestion black --bold
+set fish_color_color_comment black --bold
 set fish_color_command green
+set fish_color_cwd normal
+set fish_color_end normal
+set fish_color_error red
+set fish_color_normal normal
+set fish_color_param normal
+set fish_color_quote yellow
+
+set fish_pager_color_prefix normal
+set fish_pager_color_description black --bold
