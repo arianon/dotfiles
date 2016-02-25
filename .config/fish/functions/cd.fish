872d659473538cbde479e75e01f2f751c096f550
Michael Valera <arianon@openmailbox.org>
2016-02-24 19:33:30 -0430

    cd.fish: make it less awful

--- .config/fish/functions/cd.fish
+++ .config/fish/functions/cd.fish
@@ -1,8 +1,7 @@
-function cd
-    if test -f $argv[1]
-		set -l file $argv[1]
-        set -l dir (dirname $file)
-        echo Correcting \'$file\' to \'$dir\'
+function cd -a file
+	if test -f "$file"
+        set -l dir (dirname "$file")
+        echo "Correcting '$file' to '$dir'"
         builtin cd $dir
     else
         builtin cd $argv
