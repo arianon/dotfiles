d375877afe3a8d8f1706f748b611d1175762bbd4
Michael Valera <arianon@openmailbox.org>
2016-02-24 19:34:01 -0430

    fish: add some extra settings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,13 @@
 set -e fish_greeting
 
+set __fish_git_prompt_showdirtystate 'true'
+set __fish_git_prompt_color_branch 'yellow'
+set __fish_git_prompt_showupstream 'name'
+
+set __fish_git_prompt_char_dirtystate '!'
+set __fish_git_prompt_char_upstream_equal ''
+
+# Colors
 set fish_color_autosuggestion black --bold
 set fish_color_color_comment black --bold
 set fish_color_command green
@@ -9,6 +17,10 @@ set fish_color_error red
 set fish_color_normal normal
 set fish_color_param normal
 set fish_color_quote yellow
+set fish_color_search_match --background=blue
+set fish_color_redirection magenta
 
 set fish_pager_color_prefix normal
 set fish_pager_color_description black --bold
+
+
