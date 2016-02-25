9691f6d4c2a2bba37b59dba56bce86521ed7f325
Michael Valera <arianon@openmailbox.org>
2016-02-25 05:41:01 -0430

    fish: prompt update

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,27 +1,33 @@
-# private
-function __fish_prompt_git
-	# check if we're in a git repo
-	git rev-parse --is-inside-work-tree ^/dev/null >/dev/null
-	and echo -n " "(git rev-parse --abbrev-ref HEAD)
+function __my_fish_git_prompt
+	git rev-parse ^/dev/null
+	or return
+
+	set_color yellow
+	echo -n " "(git symbolic-ref --short HEAD)
 end
 
-#public
 function fish_prompt
-	if test $status -ne 0
+	set -l exit_code $status
+
+	if [ $USER = root ]
+		set_color green
+		echo -n "root "
+	end
+
+	if [ $exit_code -ne 0 ]
 		set_color red
 	else
 		set_color blue
 	end
 
 	echo -n "% "
-end
-
-function fish_right_prompt
-	set_color blue
-	echo -n (prompt_pwd)
-	set_color black
-	__fish_prompt_git
 	set_color normal
 end
 
+function fish_right_prompt
+	set_color $fish_color_cwd
+	echo -n (prompt_pwd)
 
+	__my_fish_git_prompt
+	set_color normal
+end
