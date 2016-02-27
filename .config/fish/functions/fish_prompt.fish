a52433860f22dd9de15e5b650e700764179695ae
Michael Valera <arianon@openmailbox.org>
2016-02-27 13:32:41 -0430

    fish: pretty print cmd duration in prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,9 +1,21 @@
-function __my_fish_git_prompt
+function prompt::git_branch
 	git rev-parse ^/dev/null
 	or return
 
 	set_color yellow
 	echo -n " "(git symbolic-ref --short HEAD)
+end
+
+function prompt::format_time -a milliseconds
+	set -l seconds (math "$milliseconds / 1000 % 60")
+	set -l minutes (math "$milliseconds / 60000 % 60")
+	set -l hours (math "$milliseconds / 3600000 % 24")
+	set -l days (math "$milliseconds / 86400000")
+
+	[ $days -gt 0 ]; and echo -n $days"d "
+	[ $hours -gt 0 ]; and echo -n $hours"h "
+	[ $minutes -gt 0 ]; and echo -n $minutes"m "
+	[ $seconds -gt 0 ]; and echo -n $seconds"s "
 end
 
 function fish_prompt
@@ -25,9 +37,14 @@ function fish_prompt
 end
 
 function fish_right_prompt
+	if [ "$CMD_DURATION" -gt 1000 ]
+		set_color yellow
+		prompt::format_time $CMD_DURATION
+	end
+
 	set_color $fish_color_cwd
 	echo -n (prompt_pwd)
 
-	__my_fish_git_prompt
+	prompt::git_branch
 	set_color normal
 end
