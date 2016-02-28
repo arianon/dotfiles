6e8fd901065d7b51b4ffbfdf23f194fff819bc4e
Michael Valera <arianon@openmailbox.org>
2016-02-28 15:33:14 -0430

    fish: config update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,7 +3,7 @@ setenv EDITOR vim
 setenv LOCAL ~/.local
 setenv GOPATH ~/.go
 
-setenv PATH ~/bin $LOCAL/bin /usr/bin /usr/sbin
+setenv PATH ~/bin $LOCAL/bin /usr/bin /usr/sbin ~/.rvm/bin
 
 if status -il
 	if [ -z "$DISPLAY" -a "$XDG_VTNR" = 1 ]
@@ -24,7 +24,7 @@ set __fish_git_prompt_char_upstream_equal ''
 set fish_color_autosuggestion black --bold
 set fish_color_color_comment black --bold
 set fish_color_command green
-set fish_color_cwd normal
+set fish_color_cwd blue
 set fish_color_end normal
 set fish_color_error red
 set fish_color_normal normal
