b70c2631b1017804c72d952c2984b8301fc10f1e
Michael Valera <arianon@openmailbox.org>
2016-02-29 11:17:21 -0430

    fish: heavily refactor my prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,50 +1,76 @@
-function prompt::git_branch
-	git rev-parse ^/dev/null
-	or return
+function prompt::git
+	set -l branch (git symbolic-ref --short HEAD)
+	set -l is_dirty (git status --porcelain --ignore-submodules ^&1)
 
-	set_color yellow
-	echo -n " "(git symbolic-ref --short HEAD)
+	# Check if the repo is dirty
+	if [ -n "$is_dirty" ]
+		set_color -o red
+		echo -n $branch!
+	else
+		set_color magenta
+		echo -n $branch
+	end
+	set_color normal
 end
 
 function prompt::format_time -a milliseconds
-	set -l seconds (math "$milliseconds / 1000 % 60")
-	set -l minutes (math "$milliseconds / 60000 % 60")
-	set -l hours (math "$milliseconds / 3600000 % 24")
-	set -l days (math "$milliseconds / 86400000")
+	[ "$milliseconds" -gt 0 ]; or return
+	which mili2human >/dev/null ^&1; or return
 
-	[ $days -gt 0 ]; and echo -n $days"d "
-	[ $hours -gt 0 ]; and echo -n $hours"h "
-	[ $minutes -gt 0 ]; and echo -n $minutes"m "
-	[ $seconds -gt 0 ]; and echo -n $seconds"s "
+	echo -n "("
+	set_color yellow
+	mili2human "$milliseconds"
+	set_color normal
+	echo -n ")"
+end
+
+function prompt::current_directory
+	set_color blue
+	echo -n "$PWD" | sed -e "s|$HOME|~|" # -e "s|\([^/.]\)[^/]*/|\1/|g"
+	set_color normal
+end
+
+function prompt::user
+	set_color green
+	echo -n "$USER"
+	set_color normal
+end
+
+function prompt::host
+	set_color red
+	echo -n (hostname)
+	set_color normal
 end
 
 function fish_prompt
 	set -l exit_code $status
+	set -l prompt_color "green"
+	set -l prompt_char '$'
 
-	if [ $USER = root ]
-		set_color green
-		echo -n "root "
+	if [ $USER = "root" ]
+		set prompt_color "magenta"
+		set prompt_char '#'
 	end
 
-	if [ $exit_code -ne 0 ]
-		set_color red
-	else
-		set_color blue
+	[ "$exit_code" -ne 0 ]; and set prompt_color "red"
+
+	if set -q SSH_CONNECTION
+		echo -n (prompt::user)" at "(prompt::host)" in "
 	end
 
-	echo -n "% "
+	prompt::current_directory
+
+	if git rev-parse ^/dev/null
+		echo -n " on "(prompt::git)
+	end
+
+	echo
+	set_color "$prompt_color"
+	echo -n "$prompt_char "
 	set_color normal
 end
 
 function fish_right_prompt
-	if [ "$CMD_DURATION" -gt 1000 ]
-		set_color yellow
-		prompt::format_time $CMD_DURATION
-	end
-
-	set_color $fish_color_cwd
-	echo -n (prompt_pwd)
-
-	prompt::git_branch
-	set_color normal
+	set -q CMD_DURATION
+	and	prompt::format_time $CMD_DURATION
 end
