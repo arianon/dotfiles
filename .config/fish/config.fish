c70af5635eb59612d2662c3bbfe09c5009a778fd
Michael Valera <arianon@openmailbox.org>
2016-04-06 13:15:38 -0430

    fish: update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,14 +1,14 @@
 set -gx BROWSER firefox
-set -gx EDITOR vim
+set -gx EDITOR emacsclient
 set -gx LOCAL ~/.local
 set -gx GOPATH ~/.go
 set -gx RUST_SRC_PATH /usr/src/rust/src
 set -gx MAIL ~/var/mail
 set -gx RBENV_SHELL fish
+set -gx GPG_TTY (tty)
 
-if [ $PATH[1] != ~/bin ]
-	set -gx PATH ~/bin $LOCAL/bin ~/.cargo/bin ~/.rbenv/bin ~/.rbenv/shims ~/.gem/ruby/2.3.0/bin $PATH
-end
+set -gx PATH ~/bin $LOCAL/bin ~/.cargo/bin ~/.rbenv/bin ~/.rbenv/shims ~/.gem/ruby/2.3.0/bin \
+             /usr/bin/{core,site,vendor}_perl /usr/bin
 
 if status --is-login
 	if [ -z "$DISPLAY" -a "$XDG_VTNR" = 1 ]
