01f84c25f0741dd1c3779a5538f7433152030bbe
Michael Valera <arianon@openmailbox.org>
2016-04-12 11:50:13 -0430

    i3: use i3bar bruh

---
+++ .i3/status.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+require 'json'
+
+def calendar
+  {
+    full_text: Time.now.strftime('%a, %d of %b')
+  }
+end
+
+def clock
+  {
+    full_text: Time.now.strftime('%X')
+  }
+end
+
+puts({ version: 1 }.to_json)
+puts '[[]'
+
+loop do
+  puts ',' << [calendar, clock].to_json
+  STDOUT.flush
+  sleep 1
+end
