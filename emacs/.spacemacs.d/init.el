fae9c549e658b809641a04f310a854d2eec3fb02
arianon <arianon@openmailbox.org>
2016-12-01 22:25:56 -0400

    remove never used spacemacs theme

--- emacs/.spacemacs.d/init.el
+++ emacs/.spacemacs.d/init.el
@@ -16,36 +16,19 @@
    dotspacemacs-configuration-layer-path '("~/.spacemacs.d")
    dotspacemacs-configuration-layers
    '(
-     gtags
-
-     php
-
-     ;; c-c++
-
-     ;; typescript
-
      auto-completion
      evil-cleverparens
-
-     ;; (spell-checking :variables
-     ;;                 spell-checking-enable-by-default nil
-     ;;                 spell-checking-enable-auto-dictionary t)
-
-     latex
      html
      javascript
      shell-scripts
      markdown
-     ;; colors
      emacs-lisp
      git
      org
      python
-     ruby
      (shell :variables
             shell-default-shell 'eshell
             shell-enable-smart-eshell t)
-     sql
      syntax-checking
      yaml
      )
@@ -64,7 +47,7 @@
    dotspacemacs-startup-lists '(recents bookmarks projects)
    dotspacemacs-themes '(spacemacs-light spacemacs-dark)
    dotspacemacs-colorize-cursor-according-to-state t
-   dotspacemacs-default-font '("Liberation Mono" :size 12 :weight normal :width normal :powerline-scale 1)
+   dotspacemacs-default-font '("tewi" :size 12 :weight normal :width normal :powerline-scale 1)
    dotspacemacs-leader-key "SPC"
    dotspacemacs-emacs-leader-key "M-m"
    dotspacemacs-major-mode-leader-key ","
@@ -97,73 +80,73 @@
 (defun dotspacemacs/user-init ()
   (setq-default custom-file "~/.spacemacs.d/.custom.el")
 
-  ;; (custom-set-variables
-  ;;  '(spacemacs-theme-comment-bg nil)
-  ;;  '(spacemacs-theme-org-highlight nil)
-  ;;  '(spacemacs-theme-org-height nil)
-  ;;  '(spacemacs-theme-custom-colors
-  ;;    '(
-  ;;      ;; UI            MINE         ORIG
-  ;;      (act1        . "#000000") ; #121212
-  ;;      (act2        . "#222222") ; #444444
-  ;;      (base        . "#ffffff") ; #b2b2b2
-  ;;      (base-dim    . "#dddddd") ; #585858
-  ;;      (bg1         . "#000000") ; #262626
-  ;;      (bg2         . "#151515") ; #1c1c1c
-  ;;      (bg3         . "#2a2a2a") ; #121212
-  ;;      (bg4         . "#333333") ; #080808
-  ;;      (border      . "#000000") ; #111111
-  ;;      (comment-bg  . "#000000") ; #262626
-  ;;      (cursor      . "#d0d0d0") ; #d0d0d0
-  ;;      (highlight   . "#222222") ; #444444
-  ;;      (lnum        . "#222222") ; #444444
-  ;;      ;; CODE
-  ;;      (comment     . "#777777") ; #008787
-  ;;      (const       . "#d54e53") ; #d75fd7
-  ;;      (err         . "#ff0000") ; #e0211d
-  ;;      (func        . "#b9ca4a") ; #d75fd7
-  ;;      (keyword     . "#7aa6da") ; #268bd2
-  ;;      (str         . "#e78c45") ; #2aa198
-  ;;      (type        . "#e7c547") ; #df005f
-  ;;      (var         . "#c397d8") ; #8787d7
-  ;;      ;; ORG
-  ;;      (cblk        . "#ffffff") ; #b2b2b2
-  ;;      (cblk-bg     . "#999999") ; #262626
-  ;;      (cblk-ln     . "#ffffff") ; #af5faf
-  ;;      (cblk-ln-bg  . "#999999") ; #333333
-  ;;      (head1       . "#268bd2") ; #268bd2
-  ;;      (head1-bg    . "#000000") ; #262626
-  ;;      (head2       . "#2aa198") ; #2aa198
-  ;;      (head2-bg    . "#000000") ; #262626
-  ;;      (head3       . "#67b11d") ; #67b11d
-  ;;      (head3-bg    . "#000000") ; #262626
-  ;;      (head4       . "#875f00") ; #875f00
-  ;;      (head4-bg    . "#000000") ; #262626
-  ;;      ;; MISC
-  ;;      (comp        . "#7aa6da") ; #d75fd7
-  ;;      (mat         . "#86dc2f") ; #86dc2f
-  ;;      (meta        . "#af875f") ; #af875f
-  ;;      (suc         . "#86dc2f") ; #86dc2f
-  ;;      (ttip        . "#888888") ; #888888
-  ;;      (ttip-bg     . "#222222") ; #444444
-  ;;      (ttip-sl     . "#333333") ; #333333
-  ;;      (war         . "#dc752f") ; #dc752f
-  ;;      ;; COLORS
-  ;;      (aqua        . "#2aa198") ; #2aa198
-  ;;      (aqua-bg     . "#000000") ; #262626
-  ;;      (green       . "#67b11d") ; #67b11d
-  ;;      (green-bg    . "#000000") ; #262626
-  ;;      (green-bg-s  . "#000000") ; #262626
-  ;;      (cyan        . "#00ffff") ; #00ffff
-  ;;      (red         . "#d70000") ; #d70000
-  ;;      (red-bg      . "#000000") ; #262626
-  ;;      (red-bg-s    . "#000000") ; #262626
-  ;;      (blue        . "#268bd2") ; #268bd2
-  ;;      (blue-bg     . "#000000") ; #262626
-  ;;      (violet      . "#af00df") ; #af00df
-  ;;      (yellow      . "#875f00") ; #875f00
-  ;;      (yellow-bg   . "#000000") ; #262626
-  ;;      )))
+  (custom-set-variables
+   '(spacemacs-theme-comment-bg nil)
+   '(spacemacs-theme-org-highlight nil)
+   '(spacemacs-theme-org-height nil)
+   '(spacemacs-theme-custom-colors
+     '(
+       ;; UI            MINE         ORIG
+       (act1        . "#000000") ; #121212
+       (act2        . "#222222") ; #444444
+       (base        . "#ffffff") ; #b2b2b2
+       (base-dim    . "#dddddd") ; #585858
+       (bg1         . "#000000") ; #262626
+       (bg2         . "#151515") ; #1c1c1c
+       (bg3         . "#2a2a2a") ; #121212
+       (bg4         . "#333333") ; #080808
+       (border      . "#000000") ; #111111
+       (comment-bg  . "#000000") ; #262626
+       (cursor      . "#d0d0d0") ; #d0d0d0
+       (highlight   . "#222222") ; #444444
+       (lnum        . "#222222") ; #444444
+       ;; CODE
+       (comment     . "#777777") ; #008787
+       (const       . "#d54e53") ; #d75fd7
+       (err         . "#ff0000") ; #e0211d
+       (func        . "#b9ca4a") ; #d75fd7
+       (keyword     . "#7aa6da") ; #268bd2
+       (str         . "#e78c45") ; #2aa198
+       (type        . "#e7c547") ; #df005f
+       (var         . "#c397d8") ; #8787d7
+       ;; ORG
+       (cblk        . "#ffffff") ; #b2b2b2
+       (cblk-bg     . "#999999") ; #262626
+       (cblk-ln     . "#ffffff") ; #af5faf
+       (cblk-ln-bg  . "#999999") ; #333333
+       (head1       . "#268bd2") ; #268bd2
+       (head1-bg    . "#000000") ; #262626
+       (head2       . "#2aa198") ; #2aa198
+       (head2-bg    . "#000000") ; #262626
+       (head3       . "#67b11d") ; #67b11d
+       (head3-bg    . "#000000") ; #262626
+       (head4       . "#875f00") ; #875f00
+       (head4-bg    . "#000000") ; #262626
+       ;; MISC
+       (comp        . "#7aa6da") ; #d75fd7
+       (mat         . "#86dc2f") ; #86dc2f
+       (meta        . "#af875f") ; #af875f
+       (suc         . "#86dc2f") ; #86dc2f
+       (ttip        . "#888888") ; #888888
+       (ttip-bg     . "#222222") ; #444444
+       (ttip-sl     . "#333333") ; #333333
+       (war         . "#dc752f") ; #dc752f
+       ;; COLORS
+       (aqua        . "#2aa198") ; #2aa198
+       (aqua-bg     . "#000000") ; #262626
+       (green       . "#67b11d") ; #67b11d
+       (green-bg    . "#000000") ; #262626
+       (green-bg-s  . "#000000") ; #262626
+       (cyan        . "#00ffff") ; #00ffff
+       (red         . "#d70000") ; #d70000
+       (red-bg      . "#000000") ; #262626
+       (red-bg-s    . "#000000") ; #262626
+       (blue        . "#268bd2") ; #268bd2
+       (blue-bg     . "#000000") ; #262626
+       (violet      . "#af00df") ; #af00df
+       (yellow      . "#875f00") ; #875f00
+       (yellow-bg   . "#000000") ; #262626
+       )))
 
   )
 
